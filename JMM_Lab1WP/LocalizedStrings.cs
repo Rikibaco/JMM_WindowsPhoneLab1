2dc941c983f0e536c81252d027292d3b1b544b34
Riste <rikibaco@outlook.com>
2013-05-21 09:38:16 +0200

    init

---
+++ JMM_Lab1WP/LocalizedStrings.cs
@@ -0,0 +1,14 @@
+﻿using JMM_Lab1WP.Resources;
+
+namespace JMM_Lab1WP
+{
+    /// <summary>
+    /// Provides access to string resources.
+    /// </summary>
+    public class LocalizedStrings
+    {
+        private static AppResources _localizedResources = new AppResources();
+
+        public AppResources LocalizedResources { get { return _localizedResources; } }
+    }
+}
